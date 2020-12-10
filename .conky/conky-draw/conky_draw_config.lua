c09cf6b0c1daba139e39e8c5c974fa8341e18c3b
cthulhu <>
2020-12-11 00:36:09 +0200

    Add conkyrc n README.md w sample.png

---
+++ .conky/conky-draw/conky_draw_config.lua
@@ -0,0 +1,79 @@
+-- Define your visual elements here
+-- For examples, and a complete list on the possible elements and their 
+-- properties, go to https://github.com/fisadev/conky-draw/
+-- (and be sure to use the lastest version)
+
+elements = {
+  {
+    kind = 'bar_graph',
+    conky_value = 'memperc',
+    from = {x = 10, y = 385},
+    to = {x = 225, y = 385},
+
+    border = true,
+
+    background_thickness = 6,
+    background_color = 0x00FF00,
+
+    bar_thickness = 6,
+    bar_color = 0x00FF55,
+
+    critical_threshold = 80,
+
+    change_color_on_critical = true,
+    background_color_critical = 0xFFA0A0,
+    bar_color_critical = 0xFF0000,
+
+    graduated = true,
+    number_graduation = 50,
+    space_between_graduation = 2,
+  },
+  {
+    kind = 'bar_graph',
+    conky_value = 'swapperc',
+    from = {x = 10, y = 425},
+    to = {x = 225, y = 425},
+
+    border = true,
+
+    background_thickness = 6,
+    background_color = 0x00FF00,
+
+    bar_thickness = 6,
+    bar_color = 0x00FF55,
+
+    critical_threshold = 80,
+
+    change_color_on_critical = true,
+    background_color_critical = 0xFFA0A0,
+    bar_color_critical = 0xFF0000,
+
+    graduated = true,
+    number_graduation = 50,
+    space_between_graduation = 2,
+  },
+  {
+    kind = 'bar_graph',
+    conky_value = 'fs_used_perc /',
+    from = {x = 10, y = 660},
+    to = {x = 225, y = 660},
+
+    border = true,
+
+    background_thickness = 6,
+    background_color = 0x00FF00,
+
+    bar_thickness = 6,
+    bar_color = 0x00FF55,
+
+    critical_threshold = 80,
+
+    change_color_on_critical = true,
+    background_color_critical = 0xFFA0A0,
+    bar_color_critical = 0xFF0000,
+
+    graduated = true,
+    number_graduation = 50,
+    space_between_graduation = 2,
+  },
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # conkyrc
 My conky config
+
+![sample.png](/sample.png)
+
+## Credits
+* [conky-draw](https://github.com/fisadev/conky-draw)
